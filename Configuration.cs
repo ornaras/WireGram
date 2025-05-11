71a6265405dc7d202cf1ab2d37abe4e818c00d6d
ornaras <ornaras.us@gmail.com>
2025-05-11 23:56:30 +0400

    Добавление настройки токена бота и id администратора

---
+++ Configuration.cs
@@ -0,0 +1,13 @@
+﻿namespace WireGram;
+
+internal static class Configuration
+{
+    public static string Token = 
+        Environment.GetEnvironmentVariable("BOT_TOKEN") ?? 
+        throw new ArgumentNullException("Env.BOT_TOKEN");
+
+    public static long AdminId = int.Parse(_adminId ?? "");
+    private readonly static string _adminId = 
+        Environment.GetEnvironmentVariable("ADMIN_ID") ??
+        throw new ArgumentNullException("Env.ADMIN_ID");
+}
