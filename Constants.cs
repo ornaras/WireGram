1a17bd8090405e064287a7efe79ffb7492fe4f15
ornaras <ornaras.us@gmail.com>
2025-05-12 00:11:18 +0400

    Добавление логирования

---
+++ Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace WireGram;
+
+internal static class Constants
+{
+    public const string LoggerFormat = "[{Timestamp:yyyy-MM-ddTHH:mm:ss.fff(zzz)} {Version}] {Level:u3} {Message:lj}{NewLine}{Exception}";
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using Serilog;
+
 namespace WireGram
 {
     public class Program
     {
+        private static void InitLogger()
+        {
+            var ver = typeof(Program).Assembly.GetName().Version!.ToString(2);
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Version", ver)
+                .WriteTo.Console(outputTemplate: Constants.LoggerFormat)
+                .WriteTo.File("/var/log/wiregram.log", outputTemplate: Constants.LoggerFormat)
+                .CreateLogger();
+        }
+
         static void Main(string[] args)
         {
+            InitLogger();
         }
     }
 }
