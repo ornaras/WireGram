2870372584dedb41f2cb2604c6213d9e5ead6eb6
ornaras <ornaras.us@gmail.com>
2025-05-12 10:09:18 +0400

    Добавлен скелет Peer

---
+++ DataBase/Peer.cs
@@ -0,0 +1,31 @@
+﻿namespace WireGram.DataBase
+{
+    internal class Peer
+    {
+        public string PublicKey { get; set; }
+        public long UserId { get; set; }
+        public DateOnly Expaired { get; set; }
+
+        public static Peer[] Get(long userId)
+        {
+            return [];
+        }
+
+        public static Peer[] Get(DateOnly expaired)
+        {
+            return [];
+        }
+
+        public static Peer Get(string publicKey)
+        {
+            return null!;
+        }
+
+        public static void Add(Peer peer)
+        {
+
+        }
+
+        public static string[] PublicKeys { get; }
+    }
+}
