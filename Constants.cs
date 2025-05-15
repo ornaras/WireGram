57ee5c2a61a2d58ff26df1e2257fa472be0d0c95
ornaras <ornaras.us@gmail.com>
2025-05-15 10:26:04 +0400

    Добавлены пути для Windows

--- Constants.cs
+++ Constants.cs
@@ -3,6 +3,10 @@
 internal static class Constants
 {
     public const string LoggerFormat = "[{Timestamp:yyyy-MM-ddTHH:mm:ss.fff(zzz)} {Version}] {Level:u3} {Message:lj}{NewLine}{Exception}";
-    public static string PeersPath = "/etc/wiregram/peers.csv";
-    public const string LogPath = "/var/log/wiregram.log";
+    public static string PeersPath = Path.Combine(IsWindows ? 
+        Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) : 
+        "/etc", "wiregram", "peers.csv");
+    public static string LogPath = Path.Combine(IsWindows ?
+        Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) :
+        "/var", "log", "wiregram.log");
 }
--- Utils.cs
+++ Utils.cs
@@ -2,6 +2,8 @@
 {
     internal static class Utils
     {
+        public static bool IsWindows => Environment.OSVersion.Platform <= PlatformID.WinCE;
+
         public static void CreateIfNotExistsDirectory(string path)
         {
             if (!Directory.Exists(path))
