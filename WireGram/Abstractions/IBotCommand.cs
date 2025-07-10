b444785966d22a7e16263500c2b10d1b7254738b
ornaras <ornaras.us@gmail.com>
2025-07-10 10:52:22 +0400

    Добавлена абстракция команд бота

---
+++ WireGram/Abstractions/IBotCommand.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot.Types;
+
+namespace WireGram.Abstractions
+{
+    internal interface IBotCommand
+    {
+        abstract static string Command { get; }
+
+        Task Execute(ChatId chat, params string[] args);
+        Task ExecuteAdmin(ChatId chat, params string[] args);
+    }
+}
