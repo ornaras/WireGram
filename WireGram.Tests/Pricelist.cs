89557724b8b18ad596621eb4284a9686313e6f6b
ornaras <ornaras.us@gmail.com>
2025-07-10 10:29:15 +0400

    Подключение тестирования

---
+++ WireGram.Tests/Pricelist.cs
@@ -0,0 +1,29 @@
+﻿namespace WireGram.Tests
+{
+    public class Pricelist
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Month()
+        {
+            var sum = WireGram.Pricelist.Calculate(1);
+            Assert.That(sum, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void Months5()
+        {
+            var sum = WireGram.Pricelist.Calculate(5);
+            Assert.That(sum, Is.EqualTo(5*63));
+        }
+
+        [Test]
+        public void Months4_13()
+        {
+            var sum = WireGram.Pricelist.Calculate(4, 13);
+            Assert.That(sum, Is.EqualTo(4*63+13*49));
+        }
+    }
+}
