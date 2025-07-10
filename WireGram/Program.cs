61bbf9f3ef7366b5abcb4fca406846460254ebc2
ornaras <ornaras.us@gmail.com>
2025-07-10 09:21:31 +0400

    Подключение Serilog

--- WireGram/Program.cs
+++ WireGram/Program.cs
@@ -1,6 +1,12 @@
+using Serilog;
 using WireGram;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSerilog(conf => conf
+    .WriteTo.File("/var/log/wiregram.log")
+    .WriteTo.Console()
+);
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
