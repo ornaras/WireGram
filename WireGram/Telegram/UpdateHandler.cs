03545b6bd682a8eaa03991c592cefb423af1cb27
ornaras <ornaras.us@gmail.com>
2025-07-10 11:33:07 +0400

    Реализация выполнения команд

--- WireGram/Telegram/UpdateHandler.cs
+++ WireGram/Telegram/UpdateHandler.cs
@@ -1,20 +1,36 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using WireGram.Abstractions;
 
 namespace WireGram.Telegram
 {
-    internal class UpdateHandler(ILogger<UpdateHandler> logger) : IUpdateHandler
+    internal partial class UpdateHandler(ILogger<UpdateHandler> logger, IConfiguration conf) : IUpdateHandler
     {
+        private readonly Dictionary<string, IBotCommand> _cmds = new()
+    {
+        };
+
         public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
         {
             logger.LogError(exception, "");
             return Task.CompletedTask;
         }
 
-        public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if(update.Message is Message msg)
+            {
+                if(msg.Text?.StartsWith('/') ?? false)
+        {
+                    var args = msg.Text.Split(' ');
+                    if (!_cmds.TryGetValue(args[0][1..], out var cmd)) return;
+                    if (msg.Chat.Id.ToString() == conf["AdminId"])
+                        await cmd.ExecuteAdmin(botClient, msg.Chat.Id, args[1..]);
+                    else
+                        await cmd.Execute(botClient, msg.Chat.Id, args[1..]);
+                }
+            }
         }
     }
 }
