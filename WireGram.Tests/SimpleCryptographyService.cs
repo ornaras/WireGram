74923caea1284606df1ca87404c39f7b33636d6c
ornaras <ornaras.us@gmail.com>
2025-07-11 18:06:54 +0400

    Добавлен тест простого криптосервиса

---
+++ WireGram.Tests/SimpleCryptographyService.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+
+namespace WireGram.Tests
+{
+    internal class SimpleCryptographyService
+    {
+        [Test]
+        public void Main()
+        {
+            var service = new Storage.SimpleCryptographyService(new Faker().Random.Hash());
+            for(var _ = 0; _ < 1000; _++)
+            {
+                var text = new Faker().Name.FullName();
+                var encrypted = service.Encrypt(text);
+                var decrypted = service.Decrypt(encrypted);
+                Assert.That(text, Is.Not.EqualTo(encrypted));
+                Assert.That(text, Is.EqualTo(decrypted));
+            }
+        }
+    }
+}
