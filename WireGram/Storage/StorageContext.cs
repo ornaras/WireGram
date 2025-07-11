42cfa3d6f0c3dba712af2d6fec3ec12df9efd2a1
ornaras <ornaras.us@gmail.com>
2025-07-11 17:51:35 +0400

    Создание контекста БД

---
+++ WireGram/Storage/StorageContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WireGram.Storage.Models;
+
+namespace WireGram.Storage
+{
+    internal class StorageContext : DbContext
+    {
+        public DbSet<Peer> Peers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=/etc/wiregram");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
