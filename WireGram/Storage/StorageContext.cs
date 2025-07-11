26ef93998b4feb4d9953dc66add8c4aa8c8ca738
ornaras <ornaras.us@gmail.com>
2025-07-11 18:05:00 +0400

    Реализация шифрования определенных столбцов таблицы БД

--- WireGram/Storage/StorageContext.cs
+++ WireGram/Storage/StorageContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WireGram.Abstractions;
+using WireGram.Attributes;
+using WireGram.Storage.Converters;
 using WireGram.Storage.Models;
 
 namespace WireGram.Storage
@@ -17,6 +19,31 @@ namespace WireGram.Storage
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            EncryptByAttribute(modelBuilder);
+        }
+
+        private void EncryptByAttribute(ModelBuilder modelBuilder)
+        {
+            var stringType = typeof(string);
+            var encryptedAttribute = typeof(EncryptedAttribute);
+            var converter = new EncryptedConverter(cryptor);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == stringType
+                        && property.PropertyInfo is not null)
+                    {
+                        var attributes = property.PropertyInfo.GetCustomAttributes(encryptedAttribute, false);
+
+                        if (attributes.Length != 0)
+                        {
+                            property.SetValueConverter(converter);
+                        }
+                    }
+                }
+            }
         }
     }
 }
