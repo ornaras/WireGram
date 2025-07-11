2a268975d76b78874e1fb8da6dd1111c86828240
ornaras <ornaras.us@gmail.com>
2025-07-11 18:05:28 +0400

    Активация шифрования столбцов таблицы Peer

--- WireGram/Storage/Models/Peer.cs
+++ WireGram/Storage/Models/Peer.cs
@@ -1,11 +1,13 @@
-﻿namespace WireGram.Storage.Models
+﻿using WireGram.Attributes;
+
+namespace WireGram.Storage.Models
 {
     internal sealed class Peer
     {
         public int Id { get; set; }
         public required string PublicKey { get; set; }
-        public required string TelegramId { get; set; }
-        public required string Address { get; set; }
-        public required string PrivateKey { get; set; }
+        [Encrypted] public required string TelegramId { get; set; }
+        [Encrypted] public required string Address { get; set; }
+        [Encrypted] public required string PrivateKey { get; set; }
     }
 }
