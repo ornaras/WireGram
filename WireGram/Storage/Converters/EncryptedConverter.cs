e0cadf18f40b0a80578d8e38973f290af6a53d6e
ornaras <ornaras.us@gmail.com>
2025-07-11 18:02:26 +0400

    Создание криптографичесого конвертера

---
+++ WireGram/Storage/Converters/EncryptedConverter.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using WireGram.Abstractions;
+
+namespace WireGram.Storage.Converters
+{
+    internal class EncryptedConverter(ICryptographyService service, ConverterMappingHints? mappingHints = null) : 
+        ValueConverter<string, string>(t => service.Encrypt(t), t => service.Decrypt(t), mappingHints);
+}
