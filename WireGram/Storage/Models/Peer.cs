80a75ef40eeaacc553b8a3b2453a18c4c8b0409f
ornaras <ornaras.us@gmail.com>
2025-07-11 17:50:38 +0400

    Реализация объекта "узел"

---
+++ WireGram/Storage/Models/Peer.cs
@@ -0,0 +1,11 @@
+﻿namespace WireGram.Storage.Models
+{
+    internal sealed class Peer
+    {
+        public int Id { get; set; }
+        public required string PublicKey { get; set; }
+        public required string TelegramId { get; set; }
+        public required string Address { get; set; }
+        public required string PrivateKey { get; set; }
+    }
+}
